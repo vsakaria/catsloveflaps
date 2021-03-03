550c85e62a91d0f94305755cc2fc98dd1db4127c
Vishal Sakaria <vsakaria@gmail.com>
2021-03-03 12:04:02 +0000

    ADD upload and preview image

--- src/components/Uploader/index.tsx
+++ src/components/Uploader/index.tsx
@@ -1,10 +1,27 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import styles from "./Uploader.module.css";
 
 const Upload = (): ReactElement => {
+
+  const [previewImage, setPreviewImage] = useState<string | null>(null)
+
+  const handleUploadChange = (event: any): void => {
+    setPreviewImage(URL.createObjectURL(event.target.files[0]))
+  }
+
   return (
     <section className={styles.main}>
       <h1 className="primary-color">Uploader</h1>
+
+      <form id="upload_form" encType="multipart/form-data">
+        <input type="file" accept="image/png, image/jpeg" onChange={handleUploadChange} />
+      </form>
+
+      {previewImage && (
+        <div>
+          <img className="preview" src={previewImage} alt="previewImage" />
+        </div>
+      )}
     </section>
   );
 };
