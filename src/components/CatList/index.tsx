5c993dd4ac1e490dec3021809bdfee0aeaeb03d1
Vishal Sakaria <vsakaria@gmail.com>
2021-03-04 20:05:39 +0000

    UPDATE CatList

--- src/components/CatList/index.tsx
+++ src/components/CatList/index.tsx
@@ -1,17 +1,45 @@
 import React, { ReactElement, useEffect } from "react";
-import { api } from "../../api";
+import { Alert, Col, Container, Row, Spinner } from "reactstrap";
+import useGetImages from "../../hooks/useGetImages/useGetImages";
+import VotingCard from "../VotingCard";
 import styles from "./CatList.module.css";
 
 const CatList = (): ReactElement => {
 
+  const [loading, errorMessage, cats, getImages] = useGetImages()
+
   useEffect(() => {
-    api.get.images()
-  })
+    if (!cats) getImages()
+  }, [cats, getImages])
 
   return (
-    <section className={styles.main}>
-      <h1 className="primary-color">React Accelertor</h1>
-    </section>
+    <Container className={styles.main}>
+      <Row>
+        <Col md="12">
+          <section>
+            <h1 className="primary-color col-md-4 d-inline-block">Cats Love Flaps</h1>
+            {loading && <Spinner className="col-md-4" color="success" style={{ width: '2rem', height: '2rem' }} type="grow" />}
+          </section>
+        </Col>
+      </Row>
+
+      <Row>
+        {cats && cats.map((cat: any) => {
+          return (
+
+            <div className="col-sm-3 col-xs-1 mb-3" key={cat.id}>
+              <VotingCard imageSrc={cat.url} id={cat.id} />
+            </div>
+          )
+        })}
+      </Row>
+
+      {errorMessage && (<Col xs="12">
+        <Alert color="danger">
+          {errorMessage}
+        </Alert>
+      </Col>)}
+    </Container >
   );
 };
 
