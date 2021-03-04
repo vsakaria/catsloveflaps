4147dadc43c723001a8ef47b21d7e35ae90107ed
Vishal Sakaria <vsakaria@gmail.com>
2021-03-04 20:09:11 +0000

    REMOVE test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./App";
-import { render } from "@testing-library/react";
-
-it("renders welcome message", () => {
-  const { getByText } = render(<App />);
-  expect(getByText('React Accelertor')).toBeInTheDocument();
-});
