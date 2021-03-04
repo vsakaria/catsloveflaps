87ffa33b2c4a53b37d4347db1299a057d28495b8
Vishal Sakaria <vsakaria@gmail.com>
2021-03-04 14:29:37 +0000

    ADD post service

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 import { post } from './post'
 
 export const baseRequest = axios.create({
-    baseURL: "https://api.thecatapi.com/v1",
-    headers: {
-        "Content-type": "application/json",
-        "x-api-key": catKey
-    }
+  baseURL: "https://api.thecatapi.com/v1",
+  headers: {
+    "Content-type": "application/json",
+    "x-api-key": catKey
+  }
 });
 
 export const api = { post }
--- src/api/post/images.ts
+++ src/api/post/images.ts
@@ -4,6 +4,5 @@ export const images = async (file: any): Promise<any> => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await baseRequest.post('/images/upload', formData);
-    return { response };
+    return baseRequest.post('/images/upload', formData)
 };
