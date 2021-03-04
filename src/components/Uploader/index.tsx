2025025ffcfbb6540267b23f5b83f94567cc903f
Vishal Sakaria <vsakaria@gmail.com>
2021-03-04 14:30:03 +0000

    ADD consume useUploader hook

--- src/components/Uploader/index.tsx
+++ src/components/Uploader/index.tsx
@@ -1,31 +1,58 @@
-import React, { ReactElement, useState } from "react";
-import { api } from "../../api";
+import React, { ReactElement, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { Alert, Col, Container, Row, Spinner } from "reactstrap";
+import useUpload from "../../hooks/useUpload/useUpload";
 import styles from "./Uploader.module.css";
 
 const Upload = (): ReactElement => {
 
-  const [previewImage, setPreviewImage] = useState<string | null>(null)
+  const history = useHistory();
+  const [file, setFile] = useState<string | null>(null)
+  const [loading, success, errorMesaage, upload] = useUpload()
 
   const handleUploadChange = (event: any): void => {
-    const file = event.target.files[0]
-    setPreviewImage(URL.createObjectURL(file))
-    api.post.images(file)
+    setFile(event.target.files[0])
   }
 
+  const handleSubmit = (event: any): void => {
+    event.preventDefault()
+    upload(file)
+  }
+
+  const onSuccessRedirect = (): void => {
+    if (success) history.push('/')
+  }
+  useEffect(onSuccessRedirect, [success])
+
   return (
-    <section className={styles.main}>
-      <h1 className="primary-color">Uploader</h1>
+    <Container>
+      <Row>
+        <Col xs="12">
+          <section className={styles.main}>
+            <h1 className="primary-color col-md-3 d-inline-block">Uploader</h1>
+            {loading && <Spinner className="col-md-4" color="success" style={{ width: '2rem', height: '2rem' }} type="grow" />}
 
-      <form id="upload_form" encType="multipart/form-data">
-        <input type="file" accept="image/png, image/jpeg" onChange={handleUploadChange} />
-      </form>
+            <Col xs="12">
+              {errorMesaage && <Alert className="" color="danger">
+                {errorMesaage}
+              </Alert>}
+            </Col>
 
-      {previewImage && (
-        <div>
-          <img className="preview" src={previewImage} alt="previewImage" />
-        </div>
-      )}
-    </section>
+
+            <form className="col-md-12 mb-5" id="upload_form" encType="multipart/form-data" onSubmit={handleSubmit}>
+              <input type="file" accept="image/png, image/jpeg" onChange={handleUploadChange} />
+              {file && <button>Upload</button>}
+            </form>
+
+            {file && (
+              <div>
+                <img className="preview col-md-6" src={URL.createObjectURL(file)} alt="previewImage" />
+              </div>
+            )}
+          </section>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
