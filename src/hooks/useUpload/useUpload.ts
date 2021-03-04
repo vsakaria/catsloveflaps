0ea10dee991bf0c241c560588f1e32f378e3c350
Vishal Sakaria <vsakaria@gmail.com>
2021-03-04 20:03:02 +0000

    UPDATE spelling

--- src/hooks/useUpload/useUpload.ts
+++ src/hooks/useUpload/useUpload.ts
@@ -3,13 +3,13 @@ import { api } from "../../api";
 
 const useUpload = (): any => {
 
-  const [errorMesaage, setErrorMesaage] = useState<string | null>(null)
+  const [errorMesaage, setErrorMessage] = useState<string | null>(null)
   const [success, setSuccess] = useState<boolean>(false)
   const [loading, setLoading] = useState<boolean>(false)
 
   const clearMessages = (): void => {
     if (loading) {
-      setErrorMesaage(null)
+      setErrorMessage(null)
       setSuccess(false)
     }
   }
@@ -24,7 +24,7 @@ const useUpload = (): any => {
       })
       .catch(error => {
         setLoading(false)
-        setErrorMesaage(error.response.data.message)
+        setErrorMessage(error.response.data.message)
       });
   }
 
