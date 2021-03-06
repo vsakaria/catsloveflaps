fa1fa8c0b180eb22b585671ae98c67257d7062ca
Vishal Sakaria <vsakaria@gmail.com>
2021-03-06 14:52:40 +0000

    ADD mock adapter and write first async test

--- src/components/CatList/CatList.test.tsx
+++ src/components/CatList/CatList.test.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React from 'react'
+import { render, waitFor, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-import { cleanup, render } from "@testing-library/react";
+import CatList from '.'
 
-import CatList from ".";
+import MockAdapter from '../../../node_modules/axios-mock-adapter'
+import { catsMock } from '../../mocks/cats'
+import { baseRequest } from '../../api'
+import { votesMock } from '../../mocks/votes'
 
-afterAll(cleanup);
+beforeEach(() => {
+  const mock = new MockAdapter(baseRequest);
 
-test("CatList displays the correct title", () => {
-  const { getByText } = render(<CatList />);
-  expect(getByText('Cats Love Flaps')).toBeInTheDocument();
+  mock
+    .onGet("/images?limit=100")
+    .reply(200, catsMock);
 
-});
+  mock
+    .onGet("/votes")
+    .reply(200, votesMock);
+})
+
+test('CatList should render title', async () => {
+  render(<CatList />)
+  await waitFor(() => screen.findByText('Cats Love Flaps'))
+  expect(screen.getByText('Cats Love Flaps')).toBeInTheDocument()
+})
+
+test('CatList should display 4 cats on the page', async () => {
+  render(<CatList />)
+  await waitFor(() => screen.findByAltText('ILJpVUPZq'))
+  expect(screen.getByAltText('ILJpVUPZq')).toBeInTheDocument()
+})
